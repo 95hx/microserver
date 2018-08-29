177d86c16c77bca1b1cd94e19dbda9a3d3adb436
hx <731191484@qq.com>
2018-08-29 22:21:31 +0800

    Add bean

--- user-provider/src/main/java/cn/luv2code/sample/userprovider/utils/DozerBeanMapperBean.java
+++ user-provider/src/main/java/cn/luv2code/sample/userprovider/utils/DozerBeanMapperBean.java
@@ -1,16 +1,26 @@
 package cn.luv2code.sample.userprovider.utils;
 
 import org.dozer.DozerBeanMapper;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
  * instance of DozerBeanMapper
  */
-@Component
+//@Component
+@Configuration
+@ImportResource()//可指定配置文件
 public class DozerBeanMapperBean {
-    @Bean
+    @Value("${server.port}")
+    private String getFromYml;
+
+    @Bean(name = "dozerBeanMapper", initMethod = "")
+    //proxyMode:容器启动时bean还没创建 通过cglib代理这个接口或者类注入到其它需要这个bean的bean中
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON,proxyMode = ScopedProxyMode.DEFAULT)
     public DozerBeanMapper getDozerBeanMapper() {
+        System.out.println(getFromYml);
         return new DozerBeanMapper();
     }
 
