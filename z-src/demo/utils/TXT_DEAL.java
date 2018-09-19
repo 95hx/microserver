84ee5fb80c4206a87fb13881ae0d4e0b704fca09
huangXin <731191484@qq.com>
2018-09-19 15:02:58 +0800

    add TXT_DEAL

---
+++ z-src/demo/utils/TXT_DEAL.java
@@ -0,0 +1,51 @@
+package xin.icec.service.entity.prana;
+
+import java.io.*;
+import java.util.Optional;
+
+/**
+ * @author hx
+ * @Title: TXT_DEAL
+ * @ProjectName frame-root
+ * @Description: TODO
+ * @date 2018/9/1914:33
+ */
+public class TXT_DEAL {
+    public static void main(String[] args) {
+        String string="";
+        try { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw
+
+            /* 读入TXT文件 */
+            if (args.length == 0) {
+                throw new FileNotFoundException();
+            }
+            String pathname = Optional.ofNullable(args[0]).orElseThrow(FileNotFoundException::new); // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
+            File filename = new File(pathname); // 要读取以上路径的input。txt文件
+            InputStreamReader reader = new InputStreamReader(
+                    new FileInputStream(filename)); // 建立一个输入流对象reader
+            BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
+            String line = "";
+            line = br.readLine();
+            while (line != null) {
+                line = br.readLine(); // 一次读入一行数据
+                string += line+"\n";
+            }
+            string=dealString(string);
+            /* 写入Txt文件 */
+            File writename = new File("output.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件
+            writename.createNewFile(); // 创建新文件
+            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+            out.write(string); // \r\n即为换行
+            out.flush(); // 把缓存区内容压入文件
+            out.close(); // 最后记得关闭文件
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String  dealString(String string) {
+
+        return string.substring(0,string.lastIndexOf("null"));
+    }
+}
