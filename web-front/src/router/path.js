ace513da2ed0a08df1fffeda5b64c712069b6941
hx <731191484@qq.com>
2018-10-04 16:52:33 +0800

    Add VideoPage with node http-server to get video

--- web-front/src/router/path.js
+++ web-front/src/router/path.js
@@ -1,12 +1,12 @@
 const LIST_USER_PAGE_PATH = '/user_list'
 const LIST_ARTICLE_PAGE_PATH = '/article_list'
 const ARTICLE_DETAIL_PAGE_PATH = '/article_detail'
+const VIDEO_PATH = '/video'
 const GET_MAIN_PAGE_PATH = '/'
 
 export function get_main_page_path() {
   return GET_MAIN_PAGE_PATH;
 }
-
 export function get_list_user_page_path() {
   return LIST_USER_PAGE_PATH;
 }
@@ -16,4 +16,7 @@ export function get_list_article_page_path() {
 }
 export function get_article_detail_page_path() {
   return ARTICLE_DETAIL_PAGE_PATH;
+}
+export function get_video_path() {
+  return VIDEO_PATH;
 }
