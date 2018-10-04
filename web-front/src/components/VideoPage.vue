9b14def153c3bcd5ccef39422fd8e13ee8c127ce
hx <731191484@qq.com>
2018-10-04 16:50:22 +0800

    Add VideoPage with node http-server to get video

---
+++ web-front/src/components/VideoPage.vue
@@ -0,0 +1,85 @@
+<template>
+  <video-player class="video-player-box"
+                ref="videoPlayer"
+                :options="playerOptions"
+                :playsinline="true"
+                customEventName="customstatechangedeventname"
+
+                @play="onPlayerPlay($event)"
+                @pause="onPlayerPause($event)"
+                @ended="onPlayerEnded($event)"
+                @waiting="onPlayerWaiting($event)"
+                @playing="onPlayerPlaying($event)"
+                @loadeddata="onPlayerLoadeddata($event)"
+                @timeupdate="onPlayerTimeupdate($event)"
+                @canplay="onPlayerCanplay($event)"
+                @canplaythrough="onPlayerCanplaythrough($event)"
+
+                @statechanged="playerStateChanged($event)"
+                @ready="playerReadied">
+  </video-player>
+
+</template>
+
+<script>
+  // require styles
+  import 'video.js/dist/video-js.css'
+
+  import {videoPlayer} from 'vue-video-player'
+
+  export default {
+    components: {
+      videoPlayer
+    },
+    data() {
+      return {
+        playerOptions: {
+          // videojs options
+          muted: false,
+          language: 'en',
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [{
+            type: "video/mp4",
+            src: "http://192.168.27.103:6666/big_buck_bunny.mp4"
+          }],
+          poster: "http://192.168.27.103:6666/container.png",
+        }
+      }
+    },
+    mounted() {
+      console.log('this is current player instance object', this.player)
+    },
+    computed: {
+      player() {
+        return this.$refs.videoPlayer.player
+      }
+    },
+    methods: {
+      // listen event
+      onPlayerPlay(player) {
+        // console.log('player play!', player)
+      },
+      onPlayerPause(player) {
+        // console.log('player pause!', player)
+      },
+      // ...player event
+
+      // or listen state event
+      playerStateChanged(playerCurrentState) {
+        // console.log('player current update state', playerCurrentState)
+      },
+
+      // player is ready
+      playerReadied(player) {
+        console.log('the player is readied', player)
+        // you can use it to do something...
+        // player.[methods]
+      }
+    }
+
+  };
+</script>
+
+<style>
+
+</style>
