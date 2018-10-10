c78f3f88010af6f2c69e31e3530e88cff2f50ef3
huangXin <731191484@qq.com>
2018-10-10 15:22:16 +0800

    Update ExtraContentToOne

--- user-provider/src/main/java/cn/luv2code/sample/userprovider/utils/demo/utils/ExtraContentToOne.java
+++ user-provider/src/main/java/cn/luv2code/sample/userprovider/utils/demo/utils/ExtraContentToOne.java
@@ -5,16 +5,17 @@ import java.util.ArrayList;
  * @author hx
  * @Title: ExtraContentToOne
  * @ProjectName micro-server-demo
- * @Description: TODO
+ * @Description: 把多个文本内容合并到一个文本
  * @date 2018/9/1917:30
  */
 public class ExtraContentToOne {
 
     public static void main(String[] args) throws Exception{
+        //文本地址
         ArrayList<String> arrayList =DirFileScan.scanFilesWithNoRecursion("C:\\Users\\18701\\Desktop\\resource");
         for (String filePath:arrayList) {
-            TxtDeal.fileToString(filePath);
+            //所有文本内容输出
+            System.out.println(TxtDeal.fileToString(filePath));
         }
-        System.out.println();
     }
 }
