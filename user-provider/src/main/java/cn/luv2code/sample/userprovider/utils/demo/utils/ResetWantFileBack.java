a52f8b1e92f0a37f9b5c0be1eeebe5b21714b944
huangXin <731191484@qq.com>
2018-10-10 17:01:18 +0800

    Update reset want file

--- user-provider/src/main/java/cn/luv2code/sample/userprovider/utils/demo/utils/ResetWantFileBack.java
+++ user-provider/src/main/java/cn/luv2code/sample/userprovider/utils/demo/utils/ResetWantFileBack.java
@@ -7,8 +7,8 @@ import java.io.File;
 import java.util.ArrayList;
 
 /**
- * 在CopyWantFile操作后
- * 把备份的文件还原回去
+ * 先使用CopyWantFile备份
+ * 然后执行本程序还原回去
  */
 public class ResetWantFileBack {
     public static void main(String[] args) throws Exception {
@@ -17,7 +17,6 @@ public class ResetWantFileBack {
             String destFile = s.substring(s.lastIndexOf("\\")).replace("=", ":").replace("~", "\\").substring(1);
             String fileName = destFile.substring(destFile.lastIndexOf("\\") + 1);
             FileCopyUtils.copy( new File(s),new File(destFile));
-            System.out.println();
         }
     }
 }
